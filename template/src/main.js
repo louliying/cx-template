2453fc9523526a138d01bd80fab7026b0f39c3d4
louliying <louliying@chexiang.com>
2018-07-12 14:55:08 +0800

    去掉miniui引用

--- template/src/main.js
+++ template/src/main.js
@@ -2,9 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import MintUI from 'mint-ui'
-import 'reset.css'
-import 'mint-ui/lib/style.css'
+// import MintUI from 'mint-ui'
+// import 'reset.css'
+// import 'mint-ui/lib/style.css'
 import router from './router'
 import {vuemaxusstore, vuemaxus} from "./vuemaxus";
 import maxusdata from "./maxusdata";
@@ -12,7 +12,7 @@ import 'es6-promise/auto'
 
 Vue.config.productionTip = false
 
-Vue.use(MintUI)
+// Vue.use(MintUI)
 Vue.use(vuemaxus)
 
 /* eslint-disable no-new */
