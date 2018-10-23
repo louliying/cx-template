9a4b613257193d922db0f92f1104fce66e4ab75a
louliying <louliying@chexiang.com>
2018-10-23 14:12:08 +0800

    update

--- template/src/config/interface.js
+++ template/src/config/interface.js
@@ -1,11 +1,9 @@
 import ajax from '../utils/ajax';
 // import baseUrl from './base';
 
-export const getInfo = async (param = {}, config = {}) =>{
-    // await ajax.get(`${baseUrl.serverB[process.env.NODE_ENV] || ''}/mobile/home/getHeadData`, param, config);
+export const getInfo = async (param = {}, config = {}) => {
     return ajax.get(`/sjdbhome/getwinlist.json`, param, config);
-}
+};
 
 export const test = async (param = {}, config = {}) =>
-    // await ajax.get(`${baseUrl.serverA[process.env.NODE_ENV] || ''}/mobile/home/getHeadData`, param, {...config, timeout: 1000});
-    ajax.get(`${baseUrl.serverA[process.env.NODE_ENV] || ''}/mobile/home/getHeadData`, param, {...config, timeout: 1000});
+    ajax.get(`/mobile/home/getHeadData`, param, config);
